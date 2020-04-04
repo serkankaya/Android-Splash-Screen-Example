5825f2d65c8c9143f002e1e95928e30411ec35d4
Serkan Kaya <serkankayatr@hotmail.com>
2020-04-04 19:58:47 +0300

    Splash Screen Example

---
+++ app/src/main/java/net/serkankaya/splashscreen/SplashScreen.java
@@ -0,0 +1,42 @@
+package net.serkankaya.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        ImageView imageView=findViewById(R.id.image_view);
+
+        Glide.with(this).load((R.drawable.splashscreen)).into(imageView);
+        startAnaEkran();
+    }
+
+    private void startAnaEkran() {
+
+        Thread git = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+
+                    startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                } catch (Exception ex) {
+                    System.out.println("Geçiş hatası " + ex.toString());
+                } finally {
+                    finish();
+                }
+            }
+        };
+        git.start();
+
+    }
+}
